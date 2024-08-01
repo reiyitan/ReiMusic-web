cf4eff46c04c2d52e9dfe5351ba7daf86e303b35
Rei Tan <tanreiyi@gmail.com>
2024-08-01 02:53:20 -0500

    set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import { LoginPage, RegisterPage, HomePage } from "./pages";
 import { PrivateRoute } from "./components";
+import { FirebaseProvider } from "./ContextProviders";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/login" element={<LoginPage />} /> 
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/home" element={<PrivateRoute redirectPath="/login"><HomePage /></PrivateRoute>} />
-            </Routes>
-        </BrowserRouter>
+            <BrowserRouter>
+                <FirebaseProvider>
+                    <Routes>
+                        <Route path="/" element={<LoginPage />} />
+                        <Route path="/login" element={<LoginPage />} /> 
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/home" element={<PrivateRoute redirectPath="/login"><HomePage /></PrivateRoute>} />
+                    </Routes>
+                </FirebaseProvider>
+            </BrowserRouter>
     )
 }
 
