39eca35df05a8859cfb022bf5c3cb1b8f76760da
Rei Tan <tanreiyi@gmail.com>
2024-08-01 02:53:56 -0500

    implement login and register pages

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,14 +2,27 @@ import { useState } from "react";
 import "../LoginRegister.css";
 import { AuthInput } from "../../components";
 import { Link } from "react-router-dom";
+import { useFirebase } from "../../ContextProviders";
 
 export const LoginPage = () => {
     const [email, setEmail] = useState<string>("");
     const [pass, setPass] = useState<string>("");
+    const [msg, setMsg] = useState<string>(""); 
+    const { login } = useFirebase();
+
+    const handleLogin = (): void => {
+        if (!email || !pass) {
+            setMsg("Fill out all fields");
+            return;
+        }
+        setMsg("");
+        login(email, pass, setMsg);
+    }
     return (
         <div className="auth-page">
+            <h1 className="auth-page-header">Reidio</h1>
             <div className="auth-form-wrapper picto-container">
-                <h1 className="auth-form-header">Login</h1>
+                <h2 className="auth-form-header">Login</h2>
                 <AuthInput 
                     label="Email"
                     value={email} 
@@ -22,8 +35,10 @@ export const LoginPage = () => {
                     setValue={setPass}
                     type="password"
                 />
+                <p className="warning-msg">{msg}</p>
                 <button
                     className="auth-page-btn"
+                    onClick={handleLogin}
                 >
                     Login
                 </button>
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,17 +1,49 @@
 import { useState } from "react";
 import "../LoginRegister.css";
 import { AuthInput } from "../../components";
+import { useFirebase } from "../../ContextProviders";
 import { Link } from "react-router-dom";
 
 export const RegisterPage = () => {
+    const [username, setUsername] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [pass, setPass] = useState<string>(""); 
     const [confPass, setConfPass] = useState<string>(""); 
+    const [msg, setMsg] = useState<string>("");
+    const { register } = useFirebase();
+
+    const handleRegister = (): void => {
+        if (!username || !email || !pass || !confPass) {
+            setMsg("Fill out all fields");
+            return;
+        }
+        if (username.trim().length < 1 || username.trim().length > 16) {
+            setMsg("Username must be 1-16 characters long");
+            return;
+        }
+        if (pass !== confPass) {
+            setMsg("Passwords do not match");
+            return;
+        }
+        if (pass.length < 6) {
+            setMsg("Password must be at least 6 characters long");
+            return;
+        }
+        setMsg(""); 
+        register(username, email, pass, setMsg);
+    }
 
     return (
         <div className="auth-page">
+            <h1 className="auth-page-header">Reidio</h1>
         <div className="auth-form-wrapper picto-container">
-            <h1 className="auth-form-header">Create an account</h1>
+            <h2 className="auth-form-header">Create an account</h2>
+            <AuthInput 
+                    label="Username"
+                    value={username} 
+                    setValue={setUsername}
+                    type="text"
+                />
             <AuthInput 
                 label="Email"
                 value={email} 
@@ -30,8 +62,10 @@ export const RegisterPage = () => {
                 setValue={setConfPass}
                 type="password"
             />
+            <p className="warning-msg">{msg}</p>
             <button
                 className="auth-page-btn"
+                onClick={handleRegister}
             >
                 Sign up
             </button>
