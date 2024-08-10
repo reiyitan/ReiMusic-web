88613d465e58e862f0c11818bac03f75e6d1f454
Rei Tan <tanreiyi@gmail.com>
2024-08-09 22:33:09 -0500

    more robust auth check

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,5 +1,5 @@
-import React from "react"; 
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react"; 
+import { useNavigate, Navigate } from "react-router-dom";
 import { useAuth } from "../../ContextProviders";
 import "./PrivateRoute.css";
 
@@ -9,11 +9,32 @@ interface PrivateRouteProps {
 }
 
 export const PrivateRoute = ({ children, redirectPath }: PrivateRouteProps) => {
+    const navigate = useNavigate();
     const auth = useAuth();
-
-    if (!auth.currentUser) {
-        return <Navigate to={redirectPath}/>
-    }
+    useEffect(() => {
+        const user = auth.currentUser;
+        if (!user) {
+            navigate(redirectPath);
+        }
+        else {
+            const checkUserInDb = async () => {
+                fetch(`http://127.0.0.1:3000/api/user/${user.uid}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json", 
+                        "Authorization": `Bearer ${await user.getIdToken(true)}`
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (!data.user) {
+                            navigate(redirectPath);
+                        }
+                    })
+            }
+            checkUserInDb();
+        }
+    }, []); 
 
     return (
         <>
