c98c9b5ea11d23a7d3d0572322a337b40d1d11a8
Rei Tan <tanreiyi@gmail.com>
2024-08-30 20:38:04 -0500

    remove unused import

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react"; 
-import { useNavigate, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../ContextProviders";
 import "./PrivateRoute.css";
 
